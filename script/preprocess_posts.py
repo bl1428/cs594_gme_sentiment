a673418e695e580c8c67269b750e2f279c00867d
Ben Luo <4107322+bl1428@users.noreply.github.com>
2021-04-04 15:56:40 -0500

    filter for posts mentioning gme

--- script/preprocess_posts.py
+++ script/preprocess_posts.py
@@ -5,17 +5,20 @@ import re
 # create dataframe, transform timestamps
 post_data = pd.read_csv('../data/reddit_wsb.csv')
 post_data = post_data[pd.to_datetime(post_data.timestamp).dt.year >= 2021]
-# print(post_data.head(3))
 
-titles = post_data[['title', 'timestamp']].copy().dropna()
-titles.title = titles.title.str.lower()
-bodies = post_data[['body', 'timestamp']].copy().dropna()
-bodies.body = bodies.body.str.lower()
+posts = post_data[['id', 'title', 'body', 'timestamp']].copy().dropna()
+posts.title = posts.title.str.lower()
 
 # remove URLs
-titles.title = titles.title.apply(lambda x: re.sub('@[^\s]+', '', x))
-bodies.body = bodies.body.apply(lambda x: re.sub('@[^\s]+', '', x))
+posts.title = posts.title.apply(lambda x: re.sub(r'@[^\s]+', '', x))
+posts.body = posts.body.apply(lambda x: re.sub(r'@[^\s]+', '', x))
 
 # remove time from timestamp
-# titles.timestamp = pd.to_datetime(titles.timestamp).dt.date
-# bodies.timestamp = pd.to_datetime(bodies.timestamp).dt.date
+# posts.timestamp = pd.to_datetime(posts.timestamp).dt.date
+
+# filter for gme mentions
+search_pattern = r'\bgme\b'
+posts = posts[(posts.title.str.contains(r'\bgme\b')) | posts.body.str.contains(r'\bgme\b')]
+
+
+print(posts.shape)
